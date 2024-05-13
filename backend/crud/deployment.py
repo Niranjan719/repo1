ddf9b7f0e83dc14eccc9feef3ccb5f9eaa1afa7b
Niranjan719 <niranjansaikoppisetti@gmail.com>
2024-05-13 18:42:33 +0530

    Ok, NOW1

--- backend/crud/deployment.py
+++ backend/crud/deployment.py
@@ -23,8 +23,7 @@ MIDDLEWARE = [
 
 
 CORS_ALLOWED_ORIGINS = [
-    'https://lively-sea-0145bb00f.5.azurestaticapps.net'
-    
+    'https://lively-sea-0145bb00f.5.azurestaticapps.net'  
 ]
 
 
--- backend/crud/settings.py
+++ backend/crud/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -119,6 +119,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_ROOT = BASE_DIR/'staticfiles'
+# STATIC_ROOT = BASE_DIR/'staticfiles'
