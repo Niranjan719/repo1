c69c3725984ed580654e2d43dd08a65b6ab7be94
Niranjan719 <niranjansaikoppisetti@gmail.com>
2024-05-13 17:51:32 +0530

    back and frontend changes.

--- backend/crud/settings.py
+++ backend/crud/settings.py
@@ -42,9 +42,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000'
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000'
+# ]
 
 ROOT_URLCONF = 'crud.urls'
 
